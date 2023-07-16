af2852b890cc9cb037a8d8c68b06864af42c2f96
Federico Amedeo Izzo <federico@izzo.pro>
2023-07-16 18:13:46 +0200

    Add mockup radio list

--- notes.md
+++ notes.md
@@ -5,4 +5,4 @@ GUI tool for flashing OpenRTX written with Rust, GTK and libadwaita.
 ## References
 
 - [GTK and Rust](https://www.gtk.org/docs/language-bindings/rust/)
-
+- [Celeste](https://github.com/hwittenborn/celeste)
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 // src/main.rs
 // Inspired by https://cxx.rs/tutorial.html
 
-use std::env;
-use std::process;
-use std::time::Duration;
-use std::thread;
 use adw::prelude::*;
-use adw::{Application, ApplicationWindow, HeaderBar};
-use gtk::{Box, Orientation, SelectionMode, Label, Button, Picture};
+use adw::{glib, Application, Leaflet, HeaderBar};
+use gtk::{ApplicationWindow, Box, ListBox, Orientation, SelectionMode, ListBoxRow, Label};
 
 const APP_ID: &str = "org.openrtx.openrtxtool";
 
@@ -20,66 +16,61 @@ mod ffi {
     }
 }
 
-fn list() {
-    ffi::list_devices();
-}
-
 fn install() {
     println!("Flashing OpenRTX firmware");
     if let Err(err) = ffi::flash_radio() {
         eprintln!("Error: {}", err);
-        process::exit(1);
+        // process::exit(1);
     }
     println!("Firmware flash completed");
     println!("Please reboot the radio");
 }
 
-fn build_ui(app: &Application) {
-    let logo = Picture::for_filename("./img/openrtx_logo.svg");
-    logo.set_alternative_text(Some("OpenRTX"));
-    logo.set_margin_top(100);
 
-    let label = Label::builder()
-        .margin_top(230)
-        .margin_end(32)
-        .margin_bottom(230)
-        .margin_start(32)
-        .label("Connect your radio")
+fn populate_device_list(list: &ListBox) -> &ListBox {
+    //let devices = ffi::list_devices();
+    let row = ListBoxRow::builder()
+        .child(
+            &Label::builder()
+            .label("Test radio")
+            .build(),
+        )
+        .build();
+    list.append(&row);
+    return list;
+}
+
+fn build_ui(app: &Application) {
+
+    let leaflet = Leaflet::builder()
         .build();
 
-    //let button = Button::builder()
-    //    .label("Flash")
-    //    .margin_top(12)
-    //    .margin_bottom(12)
-    //    .margin_start(50)
-    //    .margin_end(50)
-    //    .build();
+    let device_list = ListBox::builder()
+        .selection_mode(SelectionMode::None)
+        .build();
 
-    // Combine the content in a box
-    let content = Box::new(Orientation::Vertical, 0);
-    // Adwaitas' ApplicationWindow does not include a HeaderBar
-    content.append(&HeaderBar::new());
-    content.append(&logo);
-    content.append(&label);
-    //content.append(&button);
+    populate_device_list(&device_list);
+    leaflet.append(&device_list);
+
+    let stack_box = Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+
+    leaflet.append(&stack_box);
 
     let window: ApplicationWindow = ApplicationWindow::builder()
         .application(app)
-        .title("openrtxtool")
+        .title("OpenRTX Tool")
         .default_height(600)
         .default_width(1000)
-        .content(&content)
+        .child(&leaflet)
         .build();
 
     window.show();
 }
 
 fn main() {
-    // Spawn a new thread to monitor for new radio connections
     // TODO: switch to inotify-rs to avoid polling on Linux
-    thread::spawn(|| {
-        list();
-    });
 
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
@@ -89,26 +80,4 @@ fn main() {
 
     // Run the application
     app.run();
-    //let args: Vec<String> = env::args().collect();
-
-    //// Print usage information
-    //if args.len() < 2 {
-    //    println!("openrtxtool: OpenRTX Installation Software");
-    //    println!("usage: {} COMMAND", args[0]);
-    //    println!("commands:");
-    //    println!(" list                       List the supported connected radios");
-    //    println!(" install                    Install OpenRTX");
-    //    println!(" restore                    Restore original firmware");
-    //    process::exit(0);
-    //}
-    list();
-
-    //match args[1]
-    //    .clone()
-    //    .as_str() {
-    //    "list" => list(),
-    //    "install" => install(),
-    //    "restore" => todo!(),
-    //    _ => todo!(),
-    //}
 }
